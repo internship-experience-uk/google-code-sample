e428422aa8cd6d3dfad04c81517ac36cc28d122e
Kathrin Schuler <kathrins@google.com>
2021-06-05 22:49:16 +0100

    No more single letter variables, fix trim

--- cpp/src/helper.cpp
+++ cpp/src/helper.cpp
@@ -3,11 +3,12 @@
 #include <sstream>
 #include "helper.h"
 
-std::string trim(std::string s){
-    size_t p = s.find_first_not_of(" \t");
-    s.erase(0, p);
-    p = s.find_last_not_of(" \t");
-    if (std::string::npos != p)
-    s.erase(p+1);
-    return s;
+std::string trim(std::string toTrim){
+    size_t trimPos = toTrim.find_first_not_of(" \t");
+    toTrim.erase(0, trimPos);
+    trimPos = toTrim.find_last_not_of(" \t");
+    if (std::string::npos != trimPos) {
+        toTrim.erase(trimPos+1);
+    }
+    return toTrim;
 }
